cc0d896c01971b964cab5edbf335eb2d9954dd87
Erik Lee <eplg2n@umsystem.edu>
2020-12-21 13:29:02 -0600

    building basic navbar

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,6 @@
-import logo from './logo.svg';
 import './App.css';
 import { useEffect, useState } from 'react';
+import Navbar from './components/Navbar';
 
 import AuthHandler from './auth.js';
 
@@ -13,20 +13,7 @@ function App() {
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
       <AuthHandler onChange={setUser} />
     </div>
   );
