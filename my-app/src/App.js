8dffe796571f21b2734f9920d629931dd5132727
William Fleetwood <nanowill99@gmail.com>
2021-02-23 16:24:21 -0600

    Changed to Javascript

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,12 @@
 import './App.css';
-import { useState } from 'react';
 import Navigation from './components/Navigation';
 import Search from './components/Search';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-
-import firebase from 'firebase/app';
-
-import AuthHandler from './auth';
-import Profile from './profile';
-import EditProfile from './edit';
-import { Nav } from 'react-bootstrap';
 
 //const WORKER_ROUTE = 'http://localhost:8787';
 //const WORKER_ROUTE = 'https://tutoring_app_db.alucky0.workers.dev'
 
 function App() {
-
-  // Initialize state variables
-  const [user, setUser] = useState<firebase.User>();
-  const [profile, setProfile] = useState("");
-
   // Debugging code
   //useEffect(() => console.log(user), [user]);
 
@@ -27,21 +14,16 @@ function App() {
     <Router>
       <div className="App">
         <Navigation />
-        <AuthHandler onChange={setUser} />
         <Switch>
           <Route path="/search" component={Search} />
         </Switch>
         <br/>
         <div className="container-fluid">
-        {user ? <EditProfile user={user} /> : ""}
         </div>
         <hr />
         <div className="container-fluid">
           <div className="form-group">
-          <label htmlFor="user_id">User ID</label>
-          <input type="text" className="form-control" name="user_id" value={profile} onChange={e => setProfile(e.target.value)} />
           </div>
-          {profile ? <Profile user_id={profile} /> : ""}
         </div>
       </div>
     </Router>
