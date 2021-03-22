88bbc8d25638f6a7286a082351e21a808075c928
Jack Limes <9681747+JackLimes@users.noreply.github.com>
2021-03-22 12:39:54 -0500

    Got Img CSS to behave somewhat

--- my-app/src/components/Home.js
+++ my-app/src/components/Home.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import {Container, Row, Col} from 'react-bootstrap';
+import './Home.css'
 function Home() {
     return(
-    	<Container>
+    	<Container className="quadBox">
     		<Row>
-			<Col>Apple</Col>
-			<Col>Save your education</Col>
+			<Col className="quadrant">
+				<img className="logo" src={"/img/apple.jpg"}/>
+			</Col>
+			<Col>
+				Save your education
+			</Col>
 			</Row>
 			<Row>
 			<Col>Available Tutors</Col>
