1930f443e51deb018b9fbd91b26dafaf815d5345
Tom Dong <contact@tomdong.io>
2021-03-23 17:58:54 -0500

    Screen-sharing now supports multiple watchers

--- my-app/src/components/ScreenShareDemo.js
+++ my-app/src/components/ScreenShareDemo.js
@@ -21,8 +21,9 @@ const stunServers = {
 }
 
 /**
- * TODO: Start peer to peer broadcast
- * TODO: Start multiple peers broadcast
+ * TODO: Use one universal key to join room instead of new key for each user
+ *       This involves using something other than the callDoc ID
+ * TODO: Allow reconnect of screen share
  * TODO: Integrate demo into actual product
  * Uses WebRTC for video screen sharing and Firebase for signaling in
  * order to actually connect to video streams.
@@ -30,55 +31,59 @@ const stunServers = {
  */
 const ScreenShareDemo = () => {
   const [sharing, setSharing] = useState(false)
+  const [peerConnections, setPeerConnections] = useState({})
   const [roomID, setRoomID] = useState("")
+  const [joinKeys, setJoinKeys] = useState([])
   const [stream, setStream] = useState()
   const videoContainer = useRef(null)
 
   // Starts broadcasting and creates an offer for audience to join
   const startSharing = async () => {
+    // Initialize signaling info to database
+    const callDoc = database.ref("calls").push()
+    const callDocKey = (await callDoc).key
+    setJoinKeys([...joinKeys, callDocKey])
+    const offerCandidates = callDoc.child("offerCandidates")
+
+    // Initialize a peer connection and add it to existing peer connections
     const pc = new RTCPeerConnection(stunServers)
+    setPeerConnections({ ...peerConnections, [callDocKey]: pc })
 
-    const localStream = await navigator.mediaDevices.getDisplayMedia({
-      video: true,
-    })
-
-    // Push tracks from local stream to peer connection
+    // Get screen sharing stream from user and add it to the remote connection
+    const localStream = stream
+      ? stream
+      : await navigator.mediaDevices.getDisplayMedia({ video: true })
     localStream.getTracks().forEach((track) => {
       pc.addTrack(track, localStream)
     })
 
-    // Grab offer candidates and save to DB
-    const callDoc = database.ref("calls").push()
-    const offerCandidates = callDoc.child("offerCandidates")
-
+    // Grab host ICE candidates and save to DB
     pc.onicecandidate = (event) => {
       event.candidate && offerCandidates.push(event.candidate.toJSON())
     }
 
-    // Create offer
+    // Create an offer for a peer to join, and save it to the database
     const offerDesc = await pc.createOffer()
     await pc.setLocalDescription(offerDesc)
-
     const offer = {
       sdp: offerDesc.sdp,
       type: offerDesc.type,
     }
-
     await callDoc.set({ offer })
 
-    // Start listening for answers
+    // Start listening for peer answers, and save those answers
+    // to the signaling DB
     callDoc.on("value", async (snapshot) => {
       const data = snapshot.val()
       console.log(data)
-      if (!pc.currentRemoteDescription && data.answer) {
+      if (!pc.currentRemoteDescription && data?.answer) {
         const answerDescription = new RTCSessionDescription(data.answer)
         await pc.setRemoteDescription(answerDescription)
       }
     })
-
     const answerCandidates = callDoc.child("answerCandidates")
 
-    // Add candidate to peer connection when answered
+    // Add candidate to peer connection when they join the call
     answerCandidates.on("child_added", async (data) => {
       const candidate = new RTCIceCandidate(data.val())
       await pc.addIceCandidate(candidate)
@@ -87,42 +92,42 @@ const ScreenShareDemo = () => {
     // Finally update state if all is sucessful
     setStream(localStream)
     setSharing(true)
-    // } catch (err) {
-    //   console.error(err)
-    // }
   }
 
   // Used to join a user who's already broadcasting
   const joinSharing = async () => {
+    // Create a peer connection
     const pc = new RTCPeerConnection(stunServers)
-    // try {
     const remoteStream = new MediaStream()
 
+    // Allows remote stream to be broadcasted to user joining the room
     pc.ontrack = (event) => {
       event.streams[0].getTracks().forEach((track) => {
         remoteStream.addTrack(track)
       })
     }
 
+    // Grab signaling info for the joining room
     const callDoc = database.ref("calls").child(roomID)
     const answerCandidates = callDoc.child("answerCandidates")
 
+    // Add local ICE candidates to the signaling DB
     pc.onicecandidate = (event) => {
       event.candidate && answerCandidates.push(event.candidate.toJSON())
     }
 
+    // Get room information and 
     const callData = (await callDoc.get()).val()
-
     if (!callData) {
       throw Error("RoomID not found!")
     }
 
+    // Get host signaling information and add your own signaling information
+    // to the signaling DB
     const offerDesc = callData.offer
     await pc.setRemoteDescription(new RTCSessionDescription(offerDesc))
-
     const answerDescription = await pc.createAnswer()
     await pc.setLocalDescription(answerDescription)
-
     const answer = {
       type: answerDescription.type,
       sdp: answerDescription.sdp,
@@ -130,26 +135,30 @@ const ScreenShareDemo = () => {
 
     await callDoc.update({ answer })
 
+    // Get host ICE candidates to establish peer connection
     const offerCandidates = callDoc.child("offerCandidates")
-
     offerCandidates.on("child_added", (data) => {
       const candidate = new RTCIceCandidate(data.val())
       pc.addIceCandidate(candidate)
     })
 
-    console.log(pc.iceConnectionState)
+    // Update local state to show stream
     setStream(remoteStream)
     setSharing(true)
-    // } catch (err) {
-    //   console.error(err)
-    // }
   }
 
-  // This gets called with the broadcaster wants to stop sharing their screen
-  // TODO: This should probably cleanup signaling information from the database
+  // This gets called when the broadcaster wants to stop sharing their screen
+  // or when they disconnect from the video stream
+  // TODO: Find some way to cleanup the signaling DB when the host/audience
+  // closes the browser instead of clicking the disconnect button
   const stopSharing = () => {
     stream.getTracks().forEach((track) => track.stop())
     videoContainer.current.srcObject = null
+    // Remove signaling info from database
+    // for all peers
+    joinKeys.forEach((roomID) => {
+      database.ref("calls").child(roomID).remove()
+    })
     setSharing(false)
   }
 
@@ -186,7 +195,16 @@ const ScreenShareDemo = () => {
         playsInline
       />
       {sharing ? (
-        <button onClick={stopSharing}>Stop sharing</button>
+        <div>
+          <p>Room IDs</p>
+          <div>
+            {joinKeys.map((roomID) => (
+              <p key={roomID}>{roomID}</p>
+            ))}
+          </div>
+          <button onClick={startSharing}>Add user</button>
+          <button onClick={stopSharing}>Stop sharing</button>
+        </div>
       ) : (
         <div>
           <button onClick={startSharing}>Start screen sharing</button>
