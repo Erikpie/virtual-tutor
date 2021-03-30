d6ea88c6e2e2bc3dedb538bc18c9dc0bf8d7dd1a
Tom Dong <contact@tomdong.io>
2021-03-30 11:52:03 -0500

    Removed unused variables and addressed compile warnings

--- my-app/src/auth.tsx
+++ my-app/src/auth.tsx
@@ -1,53 +1,60 @@
-import firebase from 'firebase/app';
-import { writeUserData, setUserAdmin, setUserTutor} from './database';
-import { useEffect, useState } from 'react';
-import { auth, provider } from './firebaseInit';
-import { Navbar, Nav, Button } from 'react-bootstrap';
-
+import firebase from "firebase/app"
+import { writeUserData } from "./database"
+import { useEffect, useState } from "react"
+import { auth, provider } from "./firebaseInit"
+import { Navbar, Nav, Button } from "react-bootstrap"
 
 // TODO: FIX ANY IN ONCHANGE
 const AuthHandler = ({ onChange }: { onChange: any }) => {
+  const [user, setUser] = useState<firebase.User | null>(null)
+  useEffect(() => onChange(user), [user, onChange])
 
-    const [user, setUser] = useState<firebase.User | null>(null);
-    useEffect(() => onChange(user), [user, onChange]);
+  // Update user after an authentication changes
+  auth.onAuthStateChanged((user) => setUser(user))
 
-    // Update user after an authentication changes
-    auth.onAuthStateChanged(user => setUser(user));
+  //Making sure not to overwrite users in the db
+  if (user) {
+    var new_user = firebase.database().ref("users/" + user.uid)
+    new_user.once("value").then(function (snapshot) {
+      if (snapshot.exists()) {
+        console.log(user.email + " is already in the database")
+      } else if (user.displayName !== null && user.email !== null) {
+        writeUserData(user.uid, user.displayName, user.email, false, false)
+        console.log("stored " + user.displayName + " in database")
+      }
+    })
+  }
 
-    //Making sure not to overwrite users in the db
-    if (user){
-        var new_user = firebase.database().ref('users/' + user.uid);
-        new_user.once("value")
-        .then(function(snapshot) {
-            if (snapshot.exists()){
-                console.log(user.email + " is already in the database");
-            }
-            else if (user.displayName !== null && user.email !== null) {
-                writeUserData(user.uid, user.displayName, user.email, false, false);
-                console.log("stored "+user.displayName+" in database");
-            }
-        });
-    }
-
-return (
+  return (
     <Navbar bg="light" expand="lg">
-        <Nav className="mr-auto">
-        </Nav>
-        {user ?
-            <form className="form-inline my-2 my-lg-0">
-                <p>Logged in as {user.displayName} (uid: {user.uid})</p>
-                {user.photoURL ? <img src={user.photoURL} alt="user" /> : ''}
-                <button className="btn btn-outline-primary" type="button" onClick={() => 
-                    auth.signOut() }>Log out</button>
-            </form>
-            :
-            <form className="form-inline my-2 my-lg-0">
-                <Button variant="outline-success" type="Button" onClick={() => auth.signInWithPopup(provider)}>Sign in with Google</Button>
-            </form>
-        }
+      <Nav className="mr-auto"></Nav>
+      {user ? (
+        <form className="form-inline my-2 my-lg-0">
+          <p>
+            Logged in as {user.displayName} (uid: {user.uid})
+          </p>
+          {user.photoURL ? <img src={user.photoURL} alt="user" /> : ""}
+          <button
+            className="btn btn-outline-primary"
+            type="button"
+            onClick={() => auth.signOut()}
+          >
+            Log out
+          </button>
+        </form>
+      ) : (
+        <form className="form-inline my-2 my-lg-0">
+          <Button
+            variant="outline-success"
+            type="Button"
+            onClick={() => auth.signInWithPopup(provider)}
+          >
+            Sign in with Google
+          </Button>
+        </form>
+      )}
     </Navbar>
-    );
-};
+  )
+}
 
-export default AuthHandler;
-
+export default AuthHandler
--- my-app/src/components/Room.js
+++ my-app/src/components/Room.js
@@ -1,34 +1,35 @@
 import React from 'react';
 
-class Chatbox extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			text: "fasdf"
-		}
-	}
+// Commenting this out until these components get used
+// class Chatbox extends React.Component {
+// 	constructor(props) {
+// 		super(props);
+// 		this.state = {
+// 			text: "fasdf"
+// 		}
+// 	}
 
-	addText(newText) {
-		this.setState({text: this.state.text + newText + '\n'});
-	}
+// 	addText(newText) {
+// 		this.setState({text: this.state.text + newText + '\n'});
+// 	}
 
-	render()
-	{
-		return(
-		<label>
-		{this.state.text}
-		</label>	
-		);
-	}
-}
+// 	render()
+// 	{
+// 		return(
+// 		<label>
+// 		{this.state.text}
+// 		</label>	
+// 		);
+// 	}
+// }
 
-class Chatinput extends React.Component {
+// class Chatinput extends React.Component {
 	
-}
+// }
 
-class Chat extends React.Component {
+// class Chat extends React.Component {
 	
-}
+// }
 
 class Room extends React.Component {
 	constructor(props) {
