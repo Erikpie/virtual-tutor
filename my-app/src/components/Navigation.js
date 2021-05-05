ed28d4a24f63ebfcdf69064a89c26d9718f1539e
Daniel Johnson <dlj7m7@umsystem.edu>
2021-05-04 22:06:07 -0500

    got rid of login page

--- my-app/src/components/Navigation.js
+++ my-app/src/components/Navigation.js
@@ -1,30 +1,33 @@
 import "react-router-dom"
+import { useState } from "react"
 import { Navbar, Form, FormControl, Nav } from "react-bootstrap"
+import AuthHandler from "../auth"
 
 function Navigation() {
-  return (
-    <Navbar bg="light" expand="lg">
-      <Navbar.Brand href="/">Virtual Tutor</Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="mr-auto">
-          <Nav.Link href="/">Home</Nav.Link>
-          <Nav.Link href="/search">Search</Nav.Link>
-          <Nav.Link href="/Whiteboard">Whiteboard</Nav.Link>
-          <Nav.Link href="/login">Login</Nav.Link>
-          <Nav.Link href="/join">Join/Make a room</Nav.Link>
-        </Nav>
-        <Form inline>
-          <FormControl
-            type="text"
-            placeholder="Subject Search"
-            className="mr-sm-2"
-          />{" "}
-        </Form>
-        <Form inline></Form>
-      </Navbar.Collapse>
-    </Navbar>
-  )
-}
+  const [, setUser] = useState()
+    return (
+      <Navbar bg="light" expand="lg">
+        <Navbar.Brand href="/">Virtual Tutor</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/search">Search</Nav.Link>
+            <Nav.Link href="/Whiteboard">Whiteboard</Nav.Link>
+            <Nav.Link href="/join">Join/Make a room</Nav.Link>
+          </Nav>
+          <AuthHandler onChange={setUser} />
+          <Form inline>
+            <FormControl
+              type="text"
+              placeholder="Subject Search"
+              className="mr-sm-2"
+            />{" "}
+          </Form>
+          <Form inline></Form>
+        </Navbar.Collapse>
+      </Navbar>
+    )
+  }
 
 export default Navigation
